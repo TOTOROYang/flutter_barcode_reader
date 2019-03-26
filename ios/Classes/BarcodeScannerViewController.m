2512fb82fb17f21c0c7bd1f0e322fe5758499b60
Weicheng Yang <445919439@qq.com>
2019-03-26 13:53:40 +0800

    Get album image

--- ios/Classes/BarcodeScannerViewController.m
+++ ios/Classes/BarcodeScannerViewController.m
@@ -17,33 +17,33 @@
     self.previewView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:_previewView];
     [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"V:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
+                               constraintsWithVisualFormat:@"V:[previewView]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"previewView": _previewView}]];
     [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"H:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
-  self.scanRect = [[ScannerOverlay alloc] initWithFrame:self.view.bounds];
-  self.scanRect.translatesAutoresizingMaskIntoConstraints = NO;
-  self.scanRect.backgroundColor = UIColor.clearColor;
-  [self.view addSubview:_scanRect];
-  [self.view addConstraints:[NSLayoutConstraint
-                             constraintsWithVisualFormat:@"V:[scanRect]"
-                             options:NSLayoutFormatAlignAllBottom
-                             metrics:nil
-                             views:@{@"scanRect": _scanRect}]];
-  [self.view addConstraints:[NSLayoutConstraint
-                             constraintsWithVisualFormat:@"H:[scanRect]"
-                             options:NSLayoutFormatAlignAllBottom
-                             metrics:nil
-                             views:@{@"scanRect": _scanRect}]];
-  [_scanRect startAnimating];
+                               constraintsWithVisualFormat:@"H:[previewView]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"previewView": _previewView}]];
+    self.scanRect = [[ScannerOverlay alloc] initWithFrame:self.view.bounds];
+    self.scanRect.translatesAutoresizingMaskIntoConstraints = NO;
+    self.scanRect.backgroundColor = UIColor.clearColor;
+    [self.view addSubview:_scanRect];
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:[scanRect]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"scanRect": _scanRect}]];
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"H:[scanRect]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"scanRect": _scanRect}]];
+    [_scanRect startAnimating];
     self.scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:_previewView];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
-  [self updateFlashButton];
+    [self updateFlashButton];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -55,8 +55,8 @@
         if (success) {
             [self startScan];
         } else {
-          [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"PERMISSION_NOT_GRANTED"];
-          [self dismissViewControllerAnimated:NO completion:nil];
+            [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"PERMISSION_NOT_GRANTED"];
+            [self dismissViewControllerAnimated:NO completion:nil];
         }
     }];
 }
@@ -73,7 +73,7 @@
     NSError *error;
     [self.scanner startScanningWithResultBlock:^(NSArray<AVMetadataMachineReadableCodeObject *> *codes) {
         [self.scanner stopScanning];
-         AVMetadataMachineReadableCodeObject *code = codes.firstObject;
+        AVMetadataMachineReadableCodeObject *code = codes.firstObject;
         if (code) {
             [self.delegate barcodeScannerViewController:self didScanBarcodeWithResult:code.stringValue];
             [self dismissViewControllerAnimated:NO completion:nil];
@@ -89,15 +89,33 @@
     if (!self.hasTorch) {
         return;
     }
-    if (self.isFlashOn) {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash Off"
-                                                                                  style:UIBarButtonItemStylePlain
-                                                                                 target:self action:@selector(toggle)];
-    } else {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash On"
-                                                                                  style:UIBarButtonItemStylePlain
-                                                                                 target:self action:@selector(toggle)];
-    }
+    UIBarButtonItem *flashItem = [[UIBarButtonItem alloc] initWithTitle:@"闪光灯" style:UIBarButtonItemStylePlain target:self action:@selector(toggle)];
+    UIBarButtonItem *albumItem = [[UIBarButtonItem alloc] initWithTitle:@"相册" style:UIBarButtonItemStylePlain target:self action:@selector(pickImage)];
+    self.navigationItem.rightBarButtonItems = @[albumItem,flashItem];
+}
+
+- (void)pickImage {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    [self presentViewController:picker animated:true completion:nil];
+}
+// 选择图片成功调用此方法
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"---begin---");
+    NSLog(@"%@", info);
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    NSLog(@"----end----");
+}
+
+
+// 取消图片选择调用此方法
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    // dismiss UIImagePickerController
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)toggle {
@@ -124,7 +142,7 @@
 - (void)toggleFlash:(BOOL)on {
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (!device) return;
-
+    
     NSError *err;
     if (device.hasFlash && device.hasTorch) {
         [device lockForConfiguration:&err];
